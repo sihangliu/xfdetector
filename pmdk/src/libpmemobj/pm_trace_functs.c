0c117579442326c970694aca4df40ee1da2e6a24
xxx <xxx>
2019-12-03 11:16:22 -0500

    add pmdk

---
+++ pmdk/src/libpmemobj/pm_trace_functs.c
@@ -0,0 +1,39 @@
+#include "pm_trace_functs.h"
+
+// PM Address range manipulation
+void pm_trace_pm_addr_add(uint64_t addr, uint64_t size){
+    return;
+}
+
+void pm_trace_pm_addr_remove(uint64_t addr, uint64_t size){
+    return;
+}
+
+// Transaction annotation
+void pm_trace_tx_begin(void){
+    return;
+}
+
+void pm_trace_tx_end(void){
+    return;
+}
+
+void pm_trace_tx_addr_add(uint64_t addr, uint64_t size){
+    return;
+}
+
+void tx_commit_point(void){
+    return;
+}
+
+
+// not necessary
+/*
+void pm_trace_pmdk_funct_begin(void){
+    return;
+}
+
+void pm_trace_pmdk_funct_end(void){
+    return;
+}
+*/
---
+++ pmdk/src/libpmemobj/pm_trace_functs.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+// PM Address range manipulation
+void pm_trace_pm_addr_add(uint64_t addr, uint64_t size);
+void pm_trace_pm_addr_remove(uint64_t addr, uint64_t size);
+
+// Transaction annotation
+void pm_trace_tx_begin(void);
+void pm_trace_tx_end(void);
+void pm_trace_tx_addr_add(uint64_t addr, uint64_t size);
+
+void tx_commit_point(void);
+
+/*
+// PMDK library TX function annotation
+void pm_trace_pmdk_funct_begin(void);
+void pm_trace_pmdk_funct_end(void);
+*/
