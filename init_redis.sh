9ab00633723cd5c06e23105ad3483885a3429a83
warsier <787012293@qq.com>
2019-12-05 12:58:35 -0500

    moving patch locations

--- init_redis.sh
+++ init_redis.sh
@@ -7,15 +7,15 @@ if [[ ! -d "redis-nvml" ]]; then
 	# Change to PMDK version of Redis (PMDK was named as NVML)
 	git checkout 3.2-nvml
 	# Apply XFDetector's patch
-	git apply ../redis_xfdetector.patch
+	git apply ../patch/redis_xfdetector.patch
 	# Add PMDK to redis dependency folder
 	ln -s ../../pmdk/ ./deps/pmdk
+	# Add config for XFDetector post-failure process
+	sed -i 's/pmfile ~\/redis.pm 1gb/#pmfile ~\/redis.pm 1gb/' redis.conf
+	cp redis.conf redis_post.conf
+	sed -i 's/port 6379/port 6380/' redis_post.conf
 else
 	cd redis-nvml
 fi
 # Compile with PMDK enabled
 make USE_PMDK=yes STD=-std=gnu99
-# Add config for XFDetector post-failure process
-sed -i 's/pmfile ~\/redis.pm 1gb/#pmfile ~\/redis.pm 1gb/' redis.conf
-cp redis.conf redis_post.conf
-sed -i 's/port 6379/port 6380/' redis_post.conf
